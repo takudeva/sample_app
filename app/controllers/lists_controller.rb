9dc4128bc7ee07ef2fa1e3c83a09a0be0cc9a273
takudeva <126333608+takudeva@users.noreply.github.com>
2023-06-07 21:33:30 +0000

    [Edit]sample_app_image

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,21 +17,20 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
 
-  def edit
-    @list = List.find(params[:id])
-
-  end
-
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
